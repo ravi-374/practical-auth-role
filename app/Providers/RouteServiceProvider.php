0e4fa1fc5d1e30134f127b1900d3fa6c432f969d
ravi navadiya <ravinavadiya374@gmail.com>
2021-10-22 23:01:25 +0530

    second commit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -15,6 +15,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * This is used by Laravel authentication to redirect users after login.
      *
+     *
      * @var string
      */
     public const HOME = '/dashboard';
